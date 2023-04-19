d554ef760111a7ae5ca13cab1aa164cc31a29640
YrinaSydorko <rozaidoma@gmail.com>
2023-04-19 14:15:53 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+////const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+//galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+// rendered items
+   //function createGalleryItemsMarkup(items) {
+   //return items.map(({ preview, original, description }) => {
+     //return `<li><a class="gallery__item" href="${original}">
+     //<img
+      // class="gallery__image"
+      // src="${preview}"
+      // alt="${description}"
+     ///></a>
+ //</li>`
+   //}).join('');
+ //}
+
+/*
+var lightbox = new SimpleLightbox('.gallery a', {
+     captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+ });
+ */
+ var lightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+ });
+//console.log(galleryItems);
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
